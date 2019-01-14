cfe5410332250099900a1fb9e54165b6e86973ee
Gavin Brown <Scareb15@gmail.com>
2019-01-13 18:12:59 -0700

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Gavin Brown on 1/13/19.
+//  Copyright © 2019 DevelopITLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
